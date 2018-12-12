35c1ed1c3e8818770ad3df40580fce508f7d9278
Ege Sucu <egesucu@icloud.com>
2018-12-12 23:19:54 +0300

    first commit

--- UserChoice/ViewController.swift
+++ UserChoice/ViewController.swift
@@ -9,12 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    IBOutlet Değerleri
+    @IBOutlet weak var metinTextView: UITextView!
+    @IBOutlet weak var karanlıktemaOpsiyonu: UISwitch!
+    @IBOutlet weak var metinDüzenlemeOpsiyonu: UISwitch!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//       Butonların UserDefaults'taki kayıtlı halini aldık.
+        karanlıktemaOpsiyonu.isOn = UserDefaults.standard.bool(forKey: "karanlıkModu")
+        metinDüzenlemeOpsiyonu.isOn = UserDefaults.standard.bool(forKey: "metinDüzenleme")
+        
+//        Switchlere İzleyici atadık. Değerleri her değiştiğinde özellikler değişsin.
+        karanlıktemaOpsiyonu.addTarget(self, action: #selector(karanlıkTemaİstendi), for: .valueChanged)
+        metinDüzenlemeOpsiyonu.addTarget(self, action: #selector(metinDüzenlenebilirİstendi), for: .valueChanged)
+        
+//        Başlangıçta Bu değerler açık ise UI kendini ayarlasın
+        if karanlıktemaOpsiyonu.isOn{
+            karanlıkTemaİstendi()
+        }
+        if metinDüzenlemeOpsiyonu.isOn{
+            metinDüzenlenebilirİstendi()
+        }
+        
     }
-
+//    Karanlık temayı açıp kapatır.
+    @objc func karanlıkTemaİstendi(){
+        if karanlıktemaOpsiyonu.isOn{
+            metinTextView.textColor = UIColor.white
+            metinTextView.backgroundColor = UIColor.black
+        } else {
+            metinTextView.textColor = UIColor.black
+            metinTextView.backgroundColor = UIColor.white
+        }
+        
+    }
+//    metni düzenlenebilir yapar.
+    @objc func metinDüzenlenebilirİstendi(){
+        if metinDüzenlemeOpsiyonu.isOn{
+            metinTextView.isEditable = true
+        } else {
+            metinTextView.isEditable = false
+        }
+    }
+    
+//      IBAction Fonksiyonları
+    @IBAction func kaydetBasıldı(_ sender: Any) {
+        UserDefaults.standard.set(karanlıktemaOpsiyonu.isOn, forKey: "karanlıkModu")
+        UserDefaults.standard.set(metinDüzenlemeOpsiyonu.isOn, forKey: "metinDüzenleme")
+        UserDefaults.standard.synchronize()
+        
+    }
+    
 
 }
 
