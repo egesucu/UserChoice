2b84399e40169ff4f2de73a4074dfc4de9bdc65e
Ege Sucu <egesucu@gmail.com>
2018-12-12 22:44:34 +0300

    Initial Commit

---
+++ UserChoice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserChoice
+//
+//  Created by Ege Sucu on 12.12.2018.
+//  Copyright © 2018 Ege Sucu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
